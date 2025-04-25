e35b18ea8a9c067a016063250da45ad861c1d97c
kkase-labellevie <kkase@labellevie.jp>
2025-04-25 10:25:18 +0900

    fix: kafkaの設定変更

--- backend/src/kafka/kafka.module.ts
+++ backend/src/kafka/kafka.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { KafkaService } from './kafka.service';
+import { Partitioners } from 'kafkajs';
+import { KafkaConfig } from '@nestjs/microservices/external/kafka.interface';
 
 @Module({
   imports: [
@@ -11,6 +13,14 @@ import { KafkaService } from './kafka.service';
         options: {
           client: {
             brokers: ['localhost:9092'],
+            ssl: false,
+            sasl: undefined,
+            createPartitioner: Partitioners.LegacyPartitioner,
+          } as KafkaConfig & { createPartitioner: any },
+          consumer: {
+            groupId: 'test-group-server',
+            sessionTimeout: 10000,
+            heartbeatInterval: 3000,
           },
         },
       },
